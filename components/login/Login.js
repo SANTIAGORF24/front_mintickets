dccc3f9637c9cbf0ae2ff0e0d0559f7341d76cf6
Santiago Ramirez Forero <saramirez@mindeporte.gov.co>
2024-09-02 16:31:05 -0500

    cambios

--- components/login/Login.js
+++ components/login/Login.js
@@ -19,16 +19,13 @@ export function Login() {
   const handleLogin = async () => {
     setIsLoading(true);
     try {
-      const response = await fetch(
-        "https://backendmintickets-production.up.railway.app/auth/login",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ username, password }),
-        }
-      );
+      const response = await fetch("http://10.250.20.195:5000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
 
       const data = await response.json();
 
