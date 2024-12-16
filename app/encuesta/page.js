234b20747a607d67c8953f70e780891b4255ab71
SibartechCompany <santiforero57@gmail.com>
2024-12-16 15:11:27 -0500

    componete encuesta e image

--- app/encuesta/page.js
+++ app/encuesta/page.js
@@ -1,9 +1,17 @@
 // pages/home.js
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Encuesta from "../../components/Encuesta/Encuesta";
 
-export default function Home({ searchParams }) {
-  const { id } = searchParams;
+export default function Home() {
+  const [id, setId] = useState(null);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const idFromSearch = params.get("id");
+    setId(idFromSearch);
+  }, []);
+
+  if (!id) return <div>Loading...</div>; // or show a loading spinner
 
   return (
     <div>
